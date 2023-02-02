ca0e055350b0feae49766e096a5c4ea309232234
RutujaPatil18 <prutuja189@gmail.com>
2023-02-02 11:35:22 +0530

    [Rutuja]Add: Calculate Wages till a condition of total working hours or days is reached for a month

--- employeeUsingSwitch.js
+++ employeeUsingSwitch.js
@@ -1,26 +1,44 @@
 var empWagePerHour = 20;
+var numOfWorkingDays = 20;
 var dailyEmployeeWage;
-var fullDayHour;
-var randomValue = Math.round(Math.random() * 2);
-console.log(randomValue);
-function employee(attendance) {
-    switch (attendance) {
+var dailyHour;
+var totalWorkingDays = 0;
+var totalMonthlyWage = 0;
+var totalHrsInMonth = 100;
+var totalWorkingHrs = 0;
+function employeeDailyWage() {
+    var randomValue = Math.round(Math.random() * 2);
+    console.log(randomValue);
+    switch (randomValue) {
         case 0:
-            fullDayHour = 0;
+            dailyHour = 0;
             console.log('Employee is absent');
-            dailyEmployeeWage = empWagePerHour * fullDayHour;
+            dailyEmployeeWage = empWagePerHour * dailyHour;
+            console.log('Employee Daily Wage is :', dailyEmployeeWage);
             break;
         case 1:
-            fullDayHour = 8;
+            dailyHour = 8;
             console.log('Employee is present');
-            dailyEmployeeWage = empWagePerHour * fullDayHour;
+            dailyEmployeeWage = empWagePerHour * dailyHour;
+            console.log('Employee Daily Wage is :', dailyEmployeeWage);
             break;
         case 2:
-            fullDayHour = 4;
+            dailyHour = 4;
             console.log('Employee is present');
-            dailyEmployeeWage = empWagePerHour * fullDayHour;
+            dailyEmployeeWage = empWagePerHour * dailyHour;
+            console.log('Employee Daily Wage is :', dailyEmployeeWage);
             break;
     }
     return dailyEmployeeWage;
 }
-console.log(employee(randomValue));
+function monthlyWage() {
+    while (totalWorkingDays < numOfWorkingDays && totalWorkingHrs < totalHrsInMonth) {
+        totalWorkingDays++;
+        console.log(totalWorkingDays);
+        totalMonthlyWage = totalMonthlyWage + employeeDailyWage();
+        totalWorkingHrs = totalWorkingHrs + dailyHour;
+    }
+    console.log('Total Employee Hours :', totalWorkingHrs);
+    console.log('Employee Monthly Wage is :', totalMonthlyWage);
+}
+monthlyWage();
--- employeeUsingSwitch.ts
+++ employeeUsingSwitch.ts
@@ -1,24 +1,42 @@
 let empWagePerHour: number = 20;
+let numOfWorkingDays=20;
 var dailyEmployeeWage: number;
-var fullDayHour: number;
-var randomValue: number = Math.round(Math.random() * 2);
-console.log(randomValue)
+var dailyHour: number;
+var totalWorkingDays:number=0;
+var totalMonthlyWage:number=0;
+var totalHrsInMonth:number=100;
+var totalWorkingHrs:number=0;
 
-function employee(attendance: number) {
-    switch (attendance) {
-        case 0: fullDayHour = 0
+function employeeDailyWage():number {
+    var randomValue: number = Math.round(Math.random() * 2);
+    console.log(randomValue)
+    switch (randomValue) {
+        case 0: dailyHour = 0
             console.log('Employee is absent')
-            dailyEmployeeWage = empWagePerHour * fullDayHour
+            dailyEmployeeWage = empWagePerHour * dailyHour
+            console.log('Employee Daily Wage is :',dailyEmployeeWage)
             break;
-        case 1: fullDayHour = 8
+        case 1: dailyHour = 8
             console.log('Employee is present')
-            dailyEmployeeWage = empWagePerHour * fullDayHour
+            dailyEmployeeWage = empWagePerHour * dailyHour
+            console.log('Employee Daily Wage is :',dailyEmployeeWage)
             break;
-        case 2: fullDayHour = 4
+        case 2: dailyHour = 4
             console.log('Employee is present')
-            dailyEmployeeWage = empWagePerHour * fullDayHour
+            dailyEmployeeWage = empWagePerHour * dailyHour
+            console.log('Employee Daily Wage is :',dailyEmployeeWage)
             break;
     }
     return dailyEmployeeWage
 }
-console.log('Employee Daily Wage is :',employee(randomValue))
+function monthlyWage(){
+    while(totalWorkingDays < numOfWorkingDays && totalWorkingHrs < totalHrsInMonth ){
+        totalWorkingDays++
+        //console.log(totalWorkingDays)
+        totalMonthlyWage = totalMonthlyWage+employeeDailyWage()
+        totalWorkingHrs =totalWorkingHrs+dailyHour
+    }
+    console.log('Total Employee Hours :',totalWorkingHrs)
+    console.log('Employee Monthly Wage is :',totalMonthlyWage)
+}
+monthlyWage()
