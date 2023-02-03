71cafc01a1078da1e026bc253c58a361b43e5801
RutujaPatil18 <prutuja189@gmail.com>
2023-02-03 20:06:25 +0530

    [Rutuja] Add :save the Total Wage for Each Company using EmpWageBuilder Use Instance Variable instead of function parameters

---
+++ EmpWageBuilder.ts
@@ -0,0 +1,52 @@
+class EmpWageBuilder {
+    empWagePerHour: number;
+    numOfWorkingDays:number;
+    dailyEmployeeWage: number;
+    dailyHour: number;
+    totalWorkingDays: number;
+    totalMonthlyWage: number;
+    totalHrsInMonth: number;
+    totalWorkingHrs: number;
+    companyName:string;
+    constructor(companyName:string,empWagePerHour:number,numOfWorkingDays:number,totalHrsInMonth:number){
+        this.companyName=companyName
+        this.empWagePerHour = empWagePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.totalWorkingDays = 0;
+        this.totalMonthlyWage = 0;
+        this.totalHrsInMonth = totalHrsInMonth;
+        this.totalWorkingHrs = 0;
+    }
+    employeeDailyWage(): number {
+        var attendance = Math.round(Math.random() * 2);
+        switch (attendance) {
+            case 0: this.dailyHour = 0
+                this.dailyEmployeeWage = this.empWagePerHour * this.dailyHour
+                break;
+            case 1: this.dailyHour = 8
+                this.dailyEmployeeWage = this.empWagePerHour * this.dailyHour
+                break;
+            case 2: this.dailyHour = 4
+                this.dailyEmployeeWage = this.empWagePerHour * this.dailyHour
+                break;
+        }
+        return this.dailyEmployeeWage
+    }
+    monthlyWage():void {
+        console.log(this.companyName + ' Wage Computation')
+        while (this.totalWorkingDays < this.numOfWorkingDays && this.totalWorkingHrs < this.totalHrsInMonth) {
+            this.totalWorkingDays++
+            //console.log(this.totalWorkingDays)
+            this.totalMonthlyWage = this.totalMonthlyWage + this.employeeDailyWage()
+            this.totalWorkingHrs = this.totalWorkingHrs + this.dailyHour
+        }
+        console.log('Total Employee Hours :', this.totalWorkingHrs)
+        console.log('Employee Monthly Wage is :', this.totalMonthlyWage)
+        console.log('---------------------------------------------')
+    }
+}
+
+let company1 = new EmpWageBuilder('company1',20,20,100)
+company1.monthlyWage()
+let company2 = new EmpWageBuilder('company2',22,22,120)
+company2.monthlyWage()
